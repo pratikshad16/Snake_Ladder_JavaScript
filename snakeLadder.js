49b80966a8d95a15b5bc1a7cabf505fab5916e4b
pratikshad16 <dhagepratiksha16@gmail.com>
2020-09-26 22:03:28 +0530

    Added: Number of times dice rolled in game

--- snakeLadder.js
+++ snakeLadder.js
@@ -3,6 +3,7 @@
         const NO_PLAY = 1;
         const SNAKE = 2;
         const LADDER = 3;
+        let numberOfTimesDiceRolled=0;
         console.log("Starting Position is: " + START_POSITION)
         console.log("Ending Position is: " + END_POSITION)
 class SnakeLadder {
@@ -16,6 +17,7 @@ class SnakeLadder {
             {
             let dieNumber = this.rollDie(6) 
             let option = this.rollDie(3)
+            numberOfTimesDiceRolled++
             switch (option) {
                 case NO_PLAY:
                     console.log("You get: " + dieNumber)
@@ -23,8 +25,7 @@ class SnakeLadder {
                     START_POSITION = START_POSITION
                     console.log("You are at same position==>"+START_POSITION)
                     console.log();
-                    break;
-                
+                   
                 case SNAKE:
                     console.log("You get: " + dieNumber)
                     console.log("It's a snake option")
@@ -33,8 +34,7 @@ class SnakeLadder {
                     START_POSITION = START_POSITION + dieNumber;
                     console.log("You are at position==>" +START_POSITION)
                     console.log(); 
-                    break;                 
-
+                    
                 case LADDER:
                     console.log("You get: " + dieNumber)
                     console.log("It's a ladder option")
@@ -43,9 +43,9 @@ class SnakeLadder {
                     START_POSITION = START_POSITION - dieNumber;
                     console.log("You are at position==> " +START_POSITION)
                     console.log();
-                    break;
             }
         }
+        console.log("Number of times dice rollled is:"+numberOfTimesDiceRolled)
     }
 }
 module.exports = new SnakeLadder();
