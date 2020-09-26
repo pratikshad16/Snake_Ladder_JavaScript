e815d58d7e73f0d1a0b108063a350f4f90266f6f
pratikshad16 <dhagepratiksha16@gmail.com>
2020-09-26 11:39:52 +0530

    Added: Ability to start a game

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("Welcome to Snake And Ladder Simulation Program");
+class Main {
+    main (){
+                console.log("Welcome to Snake And Ladder Simulation Program");
+                const data = require('./snakeLadder')
+                data.startSimulation();
+            }
+}
+const obj = new Main();
+  obj.main();
---
+++ snakeLadder.js
@@ -0,0 +1,9 @@
+class SnakeLadder {
+    startSimulation(){
+        var START_POSITION = 0;
+        var END_POSITION = 100;
+        console.log("Starting Position is: "+START_POSITION)
+        console.log("Ending Position is: "+END_POSITION)
+    }
+}
+module.exports = new SnakeLadder();
