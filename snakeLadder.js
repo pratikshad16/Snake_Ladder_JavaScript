537d702d32c080354998ddbc0953d719aacfc0b0
pratikshad16 <dhagepratiksha16@gmail.com>
2020-09-26 22:31:01 +0530

    Refactored: Created functions for options

--- snakeLadder.js
+++ snakeLadder.js
@@ -3,7 +3,11 @@
         const NO_PLAY = 1;
         const SNAKE = 2;
         const LADDER = 3;
+        let dieNumber;
+        let option;
         let numberOfTimesDiceRolled=0;
+        let Player1=0;
+        let Player2=0;
         console.log("Starting Position is: " + START_POSITION)
         console.log("Ending Position is: " + END_POSITION)
 class SnakeLadder {
@@ -15,37 +19,46 @@ class SnakeLadder {
         {
             while(START_POSITION != END_POSITION)
             {
-            let dieNumber = this.rollDie(6) 
-            let option = this.rollDie(3)
+             dieNumber = this.rollDie(6) 
+             option = this.rollDie(3)
             numberOfTimesDiceRolled++
             switch (option) {
                 case NO_PLAY:
-                    console.log("You get: " + dieNumber)
-                    console.log("It's a no play option")
-                    START_POSITION = START_POSITION
-                    console.log("You are at same position==>"+START_POSITION)
-                    console.log();
+                    this.getNoPlayResult();
                    
                 case SNAKE:
-                    console.log("You get: " + dieNumber)
-                    console.log("It's a snake option")
-                    START_POSITION = START_POSITION - dieNumber;
-                    if(START_POSITION < 0) 
-                    START_POSITION = START_POSITION + dieNumber;
-                    console.log("You are at position==>" +START_POSITION)
-                    console.log(); 
+                    this.getSnakeResult();
                     
                 case LADDER:
-                    console.log("You get: " + dieNumber)
-                    console.log("It's a ladder option")
-                     START_POSITION = START_POSITION + dieNumber;
-                    if(START_POSITION > END_POSITION)
-                    START_POSITION = START_POSITION - dieNumber;
-                    console.log("You are at position==> " +START_POSITION)
-                    console.log();
+             this.getLadderResult();       
             }
         }
         console.log("Number of times dice rollled is:"+numberOfTimesDiceRolled)
+    }
+    getNoPlayResult(){
+        console.log("You get: " + dieNumber)
+        console.log("It's a no play option")
+        START_POSITION = START_POSITION
+        console.log("You are at same position==>"+START_POSITION)
+        console.log();
+    }
+    getSnakeResult(){
+        console.log("You get: " + dieNumber)
+        console.log("It's a snake option")
+        START_POSITION = START_POSITION - dieNumber;
+        if(START_POSITION < 0) 
+        START_POSITION = START_POSITION + dieNumber;
+        console.log("You are at position==>" +START_POSITION)
+        console.log(); 
+    }
+    getLadderResult(){
+        console.log("You get: " + dieNumber)
+        console.log("It's a ladder option")
+         START_POSITION = START_POSITION + dieNumber;
+        if(START_POSITION > END_POSITION)
+        START_POSITION = START_POSITION - dieNumber;
+        console.log("You are at position==> " +START_POSITION)
+        console.log();
     }
 }
 module.exports = new SnakeLadder();
