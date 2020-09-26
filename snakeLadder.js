6901204a062583b43ded5f97cf3a5c7d1a2fd7f3
pratikshad16 <dhagepratiksha16@gmail.com>
2020-09-26 11:59:16 +0530

    Added: Roll a die to get number between 1 to 6

--- snakeLadder.js
+++ snakeLadder.js
@@ -4,6 +4,13 @@ class SnakeLadder {
         var END_POSITION = 100;
         console.log("Starting Position is: "+START_POSITION)
         console.log("Ending Position is: "+END_POSITION)
+        
+    function rollDie() 
+        {
+        var dice = Math.floor((Math.random()*6)+1);
+        console.log("Number on dice is :"+dice);
+        }
+        rollDie()
     }
 }
 module.exports = new SnakeLadder();
